3f8dfc72151dfd32ce5cca09f74c1623a8a2cdf9
yoseph0524 <yoseph0524@g.ucla.edu>
2023-08-15 00:26:13 -0700

    Aug 14

--- js/greetings.js
+++ js/greetings.js
@@ -1,4 +1,4 @@
-/*const loginForm = document.querySelector("#login-form");
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
@@ -28,48 +28,5 @@ if(savedUsername === null)
 } else {
     paintGreetings(savedUsername);
 }
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/styles.css" />
-    <title>Document</title>
-  </head>
-  <body>
-    <form class="hidden" id="login-form">
-      <input
-        required
-        maxlength="15"
-        type="text"
-        placeholder="What is your name?"
-      />
-      <button>Log In</button>
-    </form>
-    <h1 id="greeting" class="hidden"></h1>
-    <h2 id="clock">00</h2>
 
-    <script src="js/greetings.js"></script>
-    <script src="js/clock.js"></script>
-  </body>
-</html>*/
 
-const button = document.querySelector("button");
-const chosen = document.querySelector("#chose");
-const result = document.querySelector("#won");
-const random = document.querySelector("#h2 input");
-const mine = document.querySelector("#h4 input");
-
-function pressedPlay(event) {
-    event.preventDefault();
-    const rand_num = Math.round(Math.random() * random.value);
-    const my_num = mine.value;
-    chosen.innerText = "You chose: " + my_num + ", the machine chose: " + rand_num + ".";
-    if (my_num != rand_num) {
-        result.innerText = "You lost!";
-      } else {
-        result.innerText = "You won!";
-      }
-}
-
-button.addEventListener("click", pressedPlay);
---
+++ js/quotes.js
@@ -0,0 +1,47 @@
+const quotes = [
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela",
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney",
+    },
+    {
+        quote: "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+        author: "Steve Jobs",
+    },
+    {
+        quote: "The future belongs to those who believe in the beauty of their dreams.",
+        author: "Eleanor Roosevelt",
+    },
+    {
+        quote: "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+        author: "Oprah Winfrey",
+    },
+    {
+        quote: "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+        author: "James Cameron",
+    },
+    {
+        quote: "You may say I'm a dreamer, but I'm not the only one. I hope someday you'll join us. And the world will live as one.",
+        author: "John Lennon",
+    },
+    {
+        quote: "You must be the change you wish to see in the world.",
+        author: "Mahatma Gandhi",
+    },
+    {
+        quote: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+        author: "Mother Teresa",
+    },
+    {
+        quote: "The only thing we have to fear is fear itself.",
+        author: "Franklin D. Roosevelt",
+    },
+]
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0]);
